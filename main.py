75cc27e349a97a2b04f9fff895bb54c37360eeec
Mason Keresty <89534948+mkjuice@users.noreply.github.com>
2022-03-22 17:51:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -18,32 +18,39 @@ async def invites(ctx):
     
     
 @bot.command(pass_context=True)
-async def upgrade(ctx): #!upgrade
+async def invite(ctx): #!upgrade
     person= ctx.message.author
     user = ctx.message.author.id
-    ref = db.reference(f"/"+str(user))    
-    status = ref.get()
-    print("status: "+ str(status))
-
+    
     rolelist=[]
     for role in person.roles:
         rolelist.append(role.name)
     print(rolelist)
-
-    if status == "nope":
-        await ctx.channel.send(str(person) + ", you haven't reached the score threshold!")
-    if int(status) <50:
-        await ctx.channel.send(str(person) + ", you haven't reached the score threshold!")
-    if int(status) >=50:
-        if "Doodle Gene Expert" not in rolelist:
-            ogrole = discord.utils.get(person.guild.roles, name = "Doodle Gene Expert")
+    
+    totalInvites = 0
+    for i in await ctx.guild.invites():
+        if i.inviter == ctx.author:
+            totalInvites += i.uses
+            
+    if totalInvites > 0:
+        if "Invite Master" not in rolelist:
+            ogrole = discord.utils.get(person.guild.roles, name = "Invite Master")
             await person.add_roles(ogrole, atomic=True)
-            await ctx.channel.send(str(person) + ", congrats! Your role is now Doodle Gene Expert.")
+            embed = discord.Embed(title = person, description = str(person) + ", congrats! You are now an Invite Master.")
+            embed.add_field(name = 'Invites: ',value = str(totalInvites), inline = False)
+            await ctx.channel.send(embed = embed)
+            
         else:
-            await ctx.channel.send(str(person) + ", You already have Doodle Gene Expert role")
-    else:        
-        await ctx.channel.send(str(person)+ ", please use the !play command to get your game code.")    
-    
-    
+            embed = discord.Embed(title = person, description = str(person) + ", you already have Invite Master role.")
+            embed.add_field(name = 'Invites: ',value = str(totalInvites), inline = False)
+            await ctx.channel.send(embed = embed)
+        
+    else:     
+        embed = discord.Embed(title = person, description= 'Not enough invites!')
+        embed.add_field(name = 'Invites: ',value = str(totalInvites), inline = False)
+        await ctx.channel.send(embed = embed)
+        
+
+
     
 client.run(TOKEN)

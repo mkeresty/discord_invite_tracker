5884256e014bf0df2d1aa3bdacdbfc2af5058132
Mason Keresty <89534948+mkjuice@users.noreply.github.com>
2022-03-22 20:16:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 import discord
 
 from discord.ext import commands
-import time
 
-TOKEN=''
+TOKEN='OTU1OTY0NTkwMzEzODQwNjYx.YjpVZw.b-SoCMYiAQP_LZ2_8yZFsQoWBB0'
 
 client = discord.Client()
 
+bot = commands.Bot(command_prefix="!", description="invite bot")
 
-@client.command()
-async def invites(ctx):
-    totalInvites = 0
-    for i in await ctx.guild.invites():
-        if i.inviter == ctx.author:
-            totalInvites += i.uses
-    await ctx.send(f"{ctx.author.mention}, you've invited {totalInvites} member{'' if totalInvites == 1 else 's'}!")
-    
-    
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+
+
 @bot.command(pass_context=True)
 async def invite(ctx): #!upgrade
     person= ctx.message.author
@@ -26,22 +23,24 @@ async def invite(ctx): #!upgrade
     for role in person.roles:
         rolelist.append(role.name)
     print(rolelist)
-    
+    x = await ctx.guild.invites()
+    print(x)
     totalInvites = 0
     for i in await ctx.guild.invites():
         if i.inviter == ctx.author:
+            print(i.inviter)
             totalInvites += i.uses
-            
-    if totalInvites > 0:
-        if "Invite Master" not in rolelist:
-            ogrole = discord.utils.get(person.guild.roles, name = "Invite Master")
+    print(totalInvites)        
+    if totalInvites > 1:
+        if "CannaDAO cultivator" not in rolelist:
+            ogrole = discord.utils.get(person.guild.roles, name = "CannaDAO cultivator")
             await person.add_roles(ogrole, atomic=True)
-            embed = discord.Embed(title = person, description = str(person) + ", congrats! You are now an Invite Master.")
+            embed = discord.Embed(title = person, description = str(person) + ", congrats! You are now an CannaDAO cultivator.")
             embed.add_field(name = 'Invites: ',value = str(totalInvites), inline = False)
             await ctx.channel.send(embed = embed)
             
         else:
-            embed = discord.Embed(title = person, description = str(person) + ", you already have Invite Master role.")
+            embed = discord.Embed(title = person, description = str(person) + ", you already have CannaDAO cultivator role.")
             embed.add_field(name = 'Invites: ',value = str(totalInvites), inline = False)
             await ctx.channel.send(embed = embed)
         
@@ -53,4 +52,5 @@ async def invite(ctx): #!upgrade
 
 
     
-client.run(TOKEN)
+#client.run(TOKEN)
+bot.run(TOKEN)   
